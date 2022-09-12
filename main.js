b2302e467e895387ae5c777424e58a74d51dba1d
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-09-12 22:54:04 +0700

    commit

--- main.js
+++ main.js
@@ -55,7 +55,8 @@ router.render = (req, res) => {
 
 // Use default router
 // Chỉ sử dụng api khi vào đường dẫn có /api
+const PORT = process.env.PORT || 3000;
 server.use('/api', router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
